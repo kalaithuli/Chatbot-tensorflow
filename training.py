c37e0129bda45faec872fdaf95667e61d2a76eb7
kalaithuli <113526296+kalaithuli@users.noreply.github.com>
2023-04-01 15:48:57 +0530

    Add files via upload

--- training.py
+++ training.py
@@ -4,9 +4,28 @@ import pickle
 import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
-#from tensorflow.keras.models import Sequential
-#from tensorflow.keras.layers import Dense, Activation, Dropout
-#from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
 from tensorflow import *
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+# so it opens the json file n reads it lyk a text n then loads it nd it returns a json object tats stored as a dictionary
+words = []
+classes = []
+documents = []
+ignore_letters = ['!','@','#','$','%','^','&','*',',','.']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        #tokenize takes a sentence n tokeizes it into words , we get a list
+        #ex - "hey how are u" is tokenized as "heh" "how" "are" "u"
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+
+
